bad2b14446816210965c06f3d3f80ffa2a79008f
Aurelian <academiacerului@gmail.com>
2017-10-07 13:46:22 +0300

    Configure Ninject for AcademiaCerului.Core project

---
+++ AcademiaCerului.Core/RepositoryModule.cs
@@ -0,0 +1,37 @@
+﻿using Ninject.Modules;
+using NHibernate;
+using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using NHibernate.Cache;
+using AcademiaCerului.Core.Objects;
+using NHibernate.Tool.hbm2ddl;
+using Ninject;
+using Ninject.Web.Common;
+
+namespace AcademiaCerului.Core
+{
+    public class RepositoryModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<ISessionFactory>()
+                .ToMethod
+                (
+                    e =>
+                        Fluently.Configure()
+                        .Database(MsSqlConfiguration.MsSql2012.ConnectionString(c =>
+                            c.FromConnectionStringWithKey("AcademiaCeruluiDbConnString")))
+                        .Cache(c => c.UseQueryCache().ProviderClass<HashtableCacheProvider>())
+                        .Mappings(m => m.FluentMappings.AddFromAssemblyOf<Post>())
+                        .ExposeConfiguration(cfg => new SchemaExport(cfg).Execute(true, true, false))
+                        .BuildConfiguration()
+                        .BuildSessionFactory()
+                )
+                .InSingletonScope();
+
+            Bind<ISession>()
+                .ToMethod((ctx) => ctx.Kernel.Get<ISessionFactory>().OpenSession())
+                .InRequestScope();
+        }
+    }
+}
