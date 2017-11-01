7bf6278698e93e42843bbf3758ccd9f97ae0d9dc
Aurelian <academiacerului@gmail.com>
2017-11-01 20:26:17 +0200

    Define a new route to get posts by category

--- AcademiaCerului/App_Start/RouteConfig.cs
+++ AcademiaCerului/App_Start/RouteConfig.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace AcademiaCerului
@@ -12,6 +8,12 @@ namespace AcademiaCerului
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                "Category",
+                "Category/{category}",
+                new {controller = "Blog", action = "GetPostsByCategory" }
+                );
 
             routes.MapRoute(
                 name: "Default",
