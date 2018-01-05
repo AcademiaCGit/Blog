f718a57cba95457eafdd2ae48bbdf535f23751ba
Aurelian <academiacerului@gmail.com>
2018-01-05 20:19:32 +0200

    Integrate Disqus

--- AcademiaCerului/Extensions.cs
+++ AcademiaCerului/Extensions.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using AcademiaCerului.Core.Objects;
+using System;
 using System.Configuration;
+using System.Web.Mvc;
 
 namespace AcademiaCerului
 {
@@ -9,6 +11,11 @@ namespace AcademiaCerului
         {
             var timezoneInfo = TimeZoneInfo.FindSystemTimeZoneById(ConfigurationManager.AppSettings["Timezone"]);
             return TimeZoneInfo.ConvertTimeFromUtc(utcDateTime, timezoneInfo).ToShortDateString();
+        }
+
+        public static string Href(this Post post, UrlHelper helper)
+        {
+            return helper.RouteUrl(new { controller = "Blog", action = "Post", year = post.PostedOn.Year, month = post.PostedOn.Month, title = post.UrlSlug });
         }
     }
 }
